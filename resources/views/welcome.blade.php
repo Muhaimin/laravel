d40e9de1b49d54cdb811cb9afd02b677ff86275d
Muhaimin Juhari <muhaiminjuhari@gmail.com>
2024-10-28 11:16:16 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel TEN</title>
+        <title>Laravel TEN 9191919</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
