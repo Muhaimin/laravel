c1f2d18fcf09ea3684d78870d3df8487d7f957d6
Muhaimin Juhari <muhaiminjuhari@gmail.com>
2025-02-26 13:56:07 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>LARAVEL 10.XX</title>
+        <title>LARAVEL SEPULUH</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
