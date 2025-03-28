8460116ed4c45e4bec635a71ad14b423aca07869
Muhaimin Juhari <muhaiminjuhari@gmail.com>
2025-03-28 10:54:20 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>LARAVEL 10.X NEW UPDATE HERE</title>
+        <title>LARAVEL 10.X</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
